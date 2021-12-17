20fe256fbb2c53c13e344fd983911f3a50a49e84
Leandro.Battisti <leandro.battisti@hbsis.com.br>
2021-12-17 12:07:08 -0300

    implementação de stdin por arquivo. implementado testes para o main

--- source/main.py
+++ source/main.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 from source.capital.service.process_capital import ProcessCapital
@@ -9,10 +10,27 @@ def read_stin():
         if line.strip().lower() == 'sair':
             sys.exit(0)
         else:
-            print(process.execute(line))
+            return process.execute(line)
+
+def by_file(file: str):
+    process = ProcessCapital()
+    f = open(file, "r")
+    value = json.dumps(json.load(f))
+    return process.execute(value)
 
 
 if __name__ == '__main__':
-   read_stin()
+    mode = sys.argv[1]
+    result = ""
+    if mode == "--interactive":
+        result = read_stin()
+    elif mode == '--server':
+        pass
+    elif "--file":
+        result = by_file(sys.argv[2])
+    sys.stdout.write(result)
+
+
+
 
 
--- tests/main.py
+++ tests/main.py
@@ -1,12 +1,24 @@
+import json
 import os
+import subprocess
+import sys
 import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        result = os.system("python ../source/main.py")
-        self.assertEqual(result, 0)
 
+    def __execute_cmd(self, cmd):
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        out, err = p.communicate()
+        return  out.decode("utf-8")
+
+
+    def test_file_mode(self):
+        result = self.__execute_cmd("python ../source/main.py --file resources/test_main_with_file.json")
+        valid_values = json.loads(result)
+        self.assertEqual(len(valid_values), 2)
+        self.assertEqual(valid_values[0]['tax'], 0)
+        self.assertEqual(valid_values[1]['tax'], 10000)
 
 if __name__ == '__main__':
     unittest.main()
